b41bb1a2595c698cfe04b1ef79bbc17a51967bf6
sanfan.hx <sanfan.hx@alibaba-inc.com>
2018-11-06 19:30:07 +0800

    update(db connect)

--- lib/common/provider.dart
+++ lib/common/provider.dart
@@ -7,24 +7,22 @@ import 'package:flutter/services.dart' show rootBundle;
 
 class Provider {
   static Database db;
+  Future connectDb() {
 
+  }
   //初始化数据库
   // isCreate 用永远 copy 一个新的数据库
   Future init(bool isCreate) async {
     String databasesPath = await getDatabasesPath();
     String path = join(databasesPath,'flutter.db');
     print("path ${path}");
-    try{
-      db = await openDatabase(path);
-    } catch (e){}
 
-    if(db == null || isCreate){
-      print('写入db');
+    if(db == null && isCreate){
       ByteData data = await rootBundle.load(join("assets", "app.db"));
       List<int> bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
       await new File(path).writeAsBytes(bytes);
-      //todo:更新数据库后修改 version 参数才会触发重新创建
-      db = await openDatabase(path,version: 4,onCreate : (Database db, int version) async{
+
+      db = await openDatabase(path,version: 2,onCreate : (Database db, int version) async{
         print('db created version is $version');
       },onOpen : (Database db) async{
         print('new db opened');
