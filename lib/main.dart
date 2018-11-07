09c509bdb7c27f760b46466d085f7fb8314d9b52
三露 <xiaojia.dxj@alibaba-inc.com>
2018-11-07 17:39:06 +0800

    first commit test

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,6 @@ import 'model/widget.dart';
 import 'package:flutter_rookie_book/components/SearchInput.dart';
 import 'common/Style.dart';
 
-
 class MyApp extends StatelessWidget {
   MyApp() {
     final router = new Router();
@@ -23,7 +22,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return new MaterialApp(
       title: 'title',
       theme: new ThemeData(
@@ -31,53 +29,51 @@ class MyApp extends StatelessWidget {
       ),
       home: new MyHomePage(),
       onGenerateRoute: Application.router.generator,
-    ) ;
+    );
   }
 }
 
 var db;
 WidgetModel widgetModel;
-void main() async{
+
+void main() async {
   final provider = new Provider();
   await provider.init(true);
   db = Provider.db;
   runApp(new MyApp());
 }
 
-
 class MyHomePage extends StatefulWidget {
   @override
-  State<StatefulWidget> createState(){
+  State<StatefulWidget> createState() {
     return _MyHomePageState();
   }
 }
 
-class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin{
-
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
   TabController controller;
   bool isSearch = false;
   String data = '无';
   String data2ThirdPage = '这是传给ThirdPage的值';
   String appBarTitle = tabData[0]['text'];
   static List tabData = [
-    {'text':'业界动态','icon': new Icon(Icons.language)},
-    {'text':'WIDGET','icon': new Icon(Icons.extension)},
-    {'text':'官网地址','icon': new Icon(Icons.home)},
-    {'text':'关于手册','icon': new Icon(Icons.favorite)}
+    {'text': '业界动态', 'icon': new Icon(Icons.language)},
+    {'text': 'WIDGET', 'icon': new Icon(Icons.extension)},
+    {'text': '官网地址', 'icon': new Icon(Icons.home)},
+    {'text': '关于手册', 'icon': new Icon(Icons.favorite)}
   ];
 
-
-
   List<Widget> myTabs = [];
-
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    controller = new TabController(initialIndex: 0, vsync: this, length:4);// 这里的length 决定有多少个底导 submenus
+    controller = new TabController(
+        initialIndex: 0, vsync: this, length: 4); // 这里的length 决定有多少个底导 submenus
     for (int i = 0; i < tabData.length; i++) {
-      myTabs.add(new Tab(text: tabData[i]['text'], icon: tabData[i]['icon'] ));
+      myTabs.add(new Tab(text: tabData[i]['text'], icon: tabData[i]['icon']));
     }
     controller.addListener(() {
       if (controller.indexIsChanging) {
@@ -91,69 +87,64 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
     controller.dispose();
     super.dispose();
   }
-  Widget buildSearchInput(){
-    return new SearchInput((value) async{
-      if(value != ''){
+
+  Widget buildSearchInput() {
+    return new SearchInput((value) async {
+      if (value != '') {
         widgetModel = new WidgetModel(db);
         List<Map> list = await widgetModel.search(value);
         print('list $list');
-        return list.map((item) => new MaterialSearchResult<String>(
-          value: item['name'],
-          text: item['name'] + '       ' + item['cnName'],
-        )).toList();
-      }else{
+        return list
+            .map((item) => new MaterialSearchResult<String>(
+                  value: item['name'],
+                  text: item['name'] + '       ' + item['cnName'],
+                ))
+            .toList();
+      } else {
         return null;
       }
-
-    },(value){},(){});
+    }, (value) {}, () {});
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-
-      appBar: new AppBar(
+        appBar: new AppBar(
 //          backgroundColor: new Color(AppColor.white),
-          title: buildSearchInput()
-      ),
-      body: new TabBarView(
-          controller: controller,
-          children: <Widget>[
-            new FirstPage(),
-            new WidgetPage(db),
-            new ThirdPage(data2ThirdPage: data2ThirdPage, callback: (val) => _onDataChange(val)),
-            new FourthPage()
-          ]
-      ),
-      bottomNavigationBar: new Material(
-        color: const Color(0xFFF0EEEF),   //底部导航栏主题颜色
-        child: new Container(
-            height:65.0,
-            child: new TabBar(
-
-            controller:controller,
-            indicatorColor: Colors.blue, //tab标签的下划线颜色
-            labelColor:const Color(0xFF000000),
-            tabs:<Tab>[
-              new Tab(text:'业界动态',icon: new Icon(Icons.language)),
-              new Tab(text:'组件',icon: new Icon(Icons.extension)),
-              new Tab(text:'官网地址',icon: new Icon(Icons.home)),
-              new Tab(text:'关于手册',icon: new Icon(Icons.favorite)),
-            ]
-          )
-        )
-      )
-    );
+            title: buildSearchInput()),
+        body: new TabBarView(controller: controller, children: <Widget>[
+          new FirstPage(),
+          new WidgetPage(db),
+          new ThirdPage(
+              data2ThirdPage: data2ThirdPage,
+              callback: (val) => _onDataChange(val)),
+          new FourthPage()
+        ]),
+        bottomNavigationBar: new Material(
+            color: const Color(0xFFF0EEEF), //底部导航栏主题颜色
+            child: new Container(
+                height: 65.0,
+                child: new TabBar(
+                    controller: controller,
+                    indicatorColor: Colors.blue, //tab标签的下划线颜色
+                    labelColor: const Color(0xFF000000),
+                    tabs: <Tab>[
+                      new Tab(text: '业界动态', icon: new Icon(Icons.language)),
+                      new Tab(text: '组件', icon: new Icon(Icons.extension)),
+                      new Tab(text: '官网地址', icon: new Icon(Icons.home)),
+                      new Tab(text: '关于手册', icon: new Icon(Icons.favorite)),
+                    ]))));
   }
+
   void _onTabChange() {
     this.setState(() {
       appBarTitle = tabData[controller.index]['text'];
     });
   }
+
   void _onDataChange(val) {
     setState(() {
       data = val;
     });
   }
 }
-
