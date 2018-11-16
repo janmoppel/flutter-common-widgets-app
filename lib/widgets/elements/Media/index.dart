828af2c91de3da1b7429b4619607d08d2d8287a1
yifeng.yl <yifeng.yl@alibaba-inc.com>
2018-11-16 15:19:57 +0800

    feat:image framework

---
+++ lib/widgets/elements/Media/index.dart
@@ -0,0 +1,13 @@
+/*
+ * @Author: 一凨 
+ * @Date: 2018-11-16 15:09:27 
+ * @Last Modified by:   一凨 
+ * @Last Modified time: 2018-11-16 15:09:27 
+ */
+import 'Image/index.dart' as Image;
+
+List getWidgets() {
+  List result = [];
+  result.addAll(Image.widgetPoints);
+  return result;
+}
