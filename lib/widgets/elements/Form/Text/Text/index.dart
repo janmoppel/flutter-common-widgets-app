31f0fb8f72806f51984e8342f8c3d948940d25bb
sanfan.hx <sanfan.hx@alibaba-inc.com>
2018-11-16 11:49:56 +0800

    add markdown suport

--- lib/widgets/elements/Form/Text/Text/index.dart
+++ lib/widgets/elements/Form/Text/Text/index.dart
@@ -1,4 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+
+const String _markdownData = """# Markdown Example
+Markdown allows you to easily include formatted text, images, and even formatted Dart code in your app.
+
+## Styling
+Style text as _italic_, __bold__, or `inline code`.
+
+- Use bulleted lists
+- To better clarify
+- Your points
+
+## Links
+You can use [hyperlinks](hyperlink) in markdown
+
+## Images
+
+You can include images:
+
+![Flutter logo](https://flutter.io/images/flutter-mark-square-100.png#100x100)
+
+## Markdown widget
+
+This is an example of how to create your own Markdown widget:
+
+    new Markdown(data: 'Hello _world_!');
+
+## Code blocks
+Formatted Dart code looks really pretty too:
+
+```
+void main() {
+  runApp(new MaterialApp(
+    home: new Scaffold(
+      body: new Markdown(data: markdownData)
+    )
+  ));
+}
+```
+
+Enjoy!
+""";
 
 class Demo extends StatefulWidget {
   static const String routeName = '/element/Form/Text/Text';
@@ -13,7 +55,7 @@ class _Demo extends State<Demo> {
           title: Text("FlatButton"),
         ),
         body: Container(
-            child: Text("this is Text")
+            child: const Markdown(data: _markdownData)
         )
     );
   }
