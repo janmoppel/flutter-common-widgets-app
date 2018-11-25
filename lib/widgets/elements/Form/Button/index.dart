f99590e057d72925e81c7da0adff72060e89a5e1
zhu.yan <zhu.yan@alibaba-inc.com>
2018-11-25 22:59:02 +0800

    Feature :完成button的所有示例

--- lib/widgets/elements/Form/Button/index.dart
+++ lib/widgets/elements/Form/Button/index.dart
@@ -5,6 +5,10 @@ import 'FlatButton/index.dart' as FlatButton;
 import 'RaisedButton/index.dart' as RaisedButton;
 import 'OutlineButton/index.dart' as OutlineButton;
 import 'IconButton/index.dart' as IconButton;
+import 'PopupMenuButton/index.dart' as PopupMenuButton;
+import 'FloatingActionButton/index.dart' as FloatingActionButton;
+import 'RawMaterialButton/index.dart' as RawMaterialButton;
+import 'DropdownButton/index.dart' as DropdownButton;
 
 
 List<WidgetPoint> widgetPoints = [
@@ -28,4 +32,24 @@ List<WidgetPoint> widgetPoints = [
     routerName: IconButton.Demo.routeName,
     buildRouter: (BuildContext context) => IconButton.Demo(),
   ),
+  WidgetPoint(
+    name: 'PopupMenuButton',
+    routerName: PopupMenuButton.Demo.routeName,
+    buildRouter: (BuildContext context) => PopupMenuButton.Demo(),
+  ),
+  WidgetPoint(
+    name: 'FloatingActionButton',
+    routerName: FloatingActionButton.Demo.routeName,
+    buildRouter: (BuildContext context) => FloatingActionButton.Demo(),
+  ),
+  WidgetPoint(
+    name: 'RawMaterialButton',
+    routerName: RawMaterialButton.Demo.routeName,
+    buildRouter: (BuildContext context) => RawMaterialButton.Demo(),
+  ),
+  WidgetPoint(
+    name: 'DropdownButton',
+    routerName: DropdownButton.Demo.routeName,
+    buildRouter: (BuildContext context) => DropdownButton.Demo(),
+  )
 ];
